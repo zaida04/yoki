fa5af98d9b990912fcf8847a558b99f713436639
Nico - Zaid <me@zaid-arshad.tech>
2020-11-30 08:22:21 -0500

    fix(suggestion): embed properly sent

--- packages/bot/src/plugins/suggestion/commands/suggestion-create.ts
+++ packages/bot/src/plugins/suggestion/commands/suggestion-create.ts
@@ -74,7 +74,8 @@ export default class suggestionCreate extends Command {
         });
 
         const sent_message = await suggestion_channel
-            .send(new SuggestionEmbed({ opener: message.author, ...created_suggestion }), {
+            .send({
+                embed: new SuggestionEmbed({ opener: message.author, ...created_suggestion }),
                 files: suggestion_message.attachments.array().map((x) => {
                     return { name: x.name ?? Date.now().toString(), attachment: x.attachment };
                 }),
