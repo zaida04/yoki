464abdbf133e98ba7c3e72317f54ce0cf5cd5a29
Nico - Zaid <me@zaid-arshad.tech>
2020-12-16 15:50:03 -0500

    bug fixes

--- packages/bot/src/common/PermissionUtil.ts
+++ packages/bot/src/common/PermissionUtil.ts
@@ -6,7 +6,8 @@ import { PermissionResolvable } from "discord.js";
 export function hasAnyPermission(member: GuildMember, permissions: PermissionResolvable[]): null | string {
     const missing: string[] = [];
     for (const permission of permissions) {
-        if (!member.hasPermission(permission)) missing.push(permission.toString());
+        if (member.hasPermission(permission)) return null;
+        missing.push(permission.toString());
     }
     return missing.length ? `missing any of these permissions: ${missing.map((x) => `\`${x}\``).join(" ")}` : null;
 }
